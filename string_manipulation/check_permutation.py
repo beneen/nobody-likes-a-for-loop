f78205c209d9349648a8f676cda9293cc3ba2833
beneen <beneen.tahir@gmail.com>
2019-03-05 17:09:01 +0000

    Extra test cases

--- string_manipulation/check_permutation.py
+++ string_manipulation/check_permutation.py
@@ -6,18 +6,13 @@ other
 """
 
 
-def get_tuples_list(text):
-    list_of_tuples = [(elem, text.count(elem)) for elem in text]
-    return list_of_tuples
+def get_tuples_set(text):
+    return set((elem, text.count(elem)) for elem in text)
 
 
 def check_permutation(text1, text2):
-    list_a = get_tuples_list(text1)
-    list_b = get_tuples_list(text2)
-    return set(list_a) == set(list_b)
+    return get_tuples_set(text1) == get_tuples_set(text2)
 
 
 if __name__ == "__main__":
-    text1 = input("insert text1:")
-    text2 = input("insert text2:")
-    print(check_permutation(text1, text2))
+    print(check_permutation(input("insert text1:"), input("insert text2:")))
--- tests/test_check_permutation.py
+++ tests/test_check_permutation.py
@@ -12,3 +12,12 @@ class TestCheck_permutation(TestCase):
 
     def test_check_permutation_empty_2(self):
         self.assertEqual(False, check_permutation("egg", ""))
+
+    def test_check_permutation_is_permutation(self):
+        self.assertEqual(True, check_permutation("egg", "gge"))
+
+    def test_check_permutation_not_permutation(self):
+        self.assertEqual(False, check_permutation("egg", "not"))
+
+    def test_check_permutation_repeated_characters(self):
+        self.assertEqual(False, check_permutation("egg", "eggg"))
